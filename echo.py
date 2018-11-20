0838ec0e12eeb70028fac58bfca69a890144ffd7
Piero Madar <madarp@pobox.com>
2018-11-19 19:59:10 -0500

    Add ref soln and starter file contents.

---
+++ echo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""An enhanced version of the 'echo' cmd line utility"""
+
+__author__ = "???"
+
+
+import sys
+
+
+def create_parser():
+    """Creates and returns an argparse cmd line option parser"""
+    pass
+
+
+def main(args):
+    """Implementation of echo"""
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ tests/test_echo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import echo
+
+# Your test case class goes here
+
+
+if __name__ == '__main__':
+    unittest.main()
