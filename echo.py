81e8739b8814b5b731269e90840e3367bd4d4ea8
Michael Gabbard <zanvoysan@gmail.com>
2020-03-06 22:32:52 -0500

    done

--- echo.py
+++ echo.py
@@ -6,17 +6,30 @@ __author__ = "???"
 
 
 import sys
+import argparse
 
 
 def create_parser():
-    """Creates and returns an argparse cmd line option parser"""
-    pass
-
+    parser = argparse.ArgumentParser(description='Perform transformation on input text.')
+    parser.add_argument('-u', '--upper', action='store_true', help='convert text to uppercase')
+    parser.add_argument('-l', '--lower', action='store_true', help='convert text to lowercase')
+    parser.add_argument('-t', '--title', action='store_true',  help='convert text to titlecase')
+    parser.add_argument('text', help='text to be manipulated')
+    return parser
 
 def main(args):
-    """Implementation of echo"""
-    pass
+    parser = create_parser()
+    ns = parser.parse_args(args)
+
+    output = ns.text
+    if ns.upper == True:
+        output = output.upper()
+    if ns.lower == True:
+        output = output.lower()
+    if ns.title == True:
+        output = output.title()
+    return output
 
 
 if __name__ == '__main__':
-    pass
+    main(sys.argv[1:])
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -3,10 +3,96 @@
 
 import unittest
 import echo
+import subprocess
+
 
 # Your test case class goes here
 class TestEcho(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.parser = echo.create_parser()
+    
+    def test_no_option(self):
+        args = ['hElLo WoRlD']
+        ns = self.parser.parse_args(args)
+        self.assertFalse(ns.upper)
+        self.assertFalse(ns.lower)
+        self.assertFalse(ns.title)
+        actual = echo.main(args)
+        excepted = 'hElLo WoRlD'
+        self.assertEqual(actual, excepted)
+
+    def test_all_options(self):
+        args = ['-tlu', 'hElLo WoRlD']
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
+        self.assertTrue(ns.lower)
+        self.assertTrue(ns.title)
+
+        actual = echo.main(args)
+        excepted = 'Hello World'
+        self.assertEqual(actual, excepted)
+
+    def test_upper_short(self):
+        args = ['-u', 'hElLo WoRlD']
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
+
+        actual = echo.main(args)
+        excepted = 'HELLO WORLD'
+        self.assertEqual(actual, excepted)
+    def test_lower_short(self):
+        args = ['-l', 'hElLo WoRlD']
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.lower)
+
+        actual = echo.main(args)
+        excepted = 'hello world'
+        self.assertEqual(actual, excepted)
+    def test_title_short(self):
+        args = ['-t', 'hElLo WoRlD']
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.title)
+
+        actual = echo.main(args)
+        excepted = 'Hello World'
+        self.assertEqual(actual, excepted)
+    def test_upper_long(self):
+        args = ['--upper', 'hElLo WoRlD']
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
+
+        actual = echo.main(args)
+        excepted = 'HELLO WORLD'
+        self.assertEqual(actual, excepted)
+    def test_lower_long(self):
+        args = ['--lower', 'hElLo WoRlD']
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.lower)
+
+        actual = echo.main(args)
+        excepted = 'hello world'
+        self.assertEqual(actual, excepted)
+    def test_title_long(self):
+        args = ['--title', 'hElLo WoRlD']
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.title)
+
+        actual = echo.main(args)
+        excepted = 'Hello World'
+        self.assertEqual(actual, excepted)
+
+    def test_help(self):
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout = stdout.decode('utf8')
+        with open("./USAGE") as f:
+            usage = f.read()
+            
+        self.assertEquals(stdout, usage)
+    
+
 
 if __name__ == '__main__':
     unittest.main()
